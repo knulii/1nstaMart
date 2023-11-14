0ce75e214eafa3cb077a94034de3e323dced1cf2
anno <anno@kali.kali.yerbas.org>
2023-11-13 23:46:24 -0800

    Next

--- finish7.sh
+++ finish7.sh
@@ -2,12 +2,11 @@
 
 sleep 1
 echo "Lets finish this"
-
+sudo tor
 sleep 5
 
 sudo chmod -R uga+rwx /home/user1/1nstaMart
 sudo mv .env /var/www/eckmar/
-
 wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0.deb
 sudo dpkg -i elasticsearch-6.0.0.deb
 
@@ -42,11 +41,7 @@ sudo chmod -R 755 /var/www/eckmar/storage/public/products
 sudo chgrp -R www-data /var/www/eckmar/storage/public/products
 sudo chmod -R ug+rwx /var/www/eckmar/storage/public/products
 
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
-sudo mv composer.phar /usr/local/bin/composer
+
 composer update
 composer install
 npm install
--- master.sh
+++ master.sh
@@ -47,6 +47,14 @@ echo "JAVA_HOME=$(which java)" | sudo tee -a /etc/environment
 source /etc/environment
 echo $JAVA_HOME
 
+cd eckmar 
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+cd ..
+
 sudo mv eckmar /var/www/eckmar/
 
 sudo rm -r /etc/nginx/sites-available/default
