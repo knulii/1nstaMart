9bb0de8620d251f62aac0f2c8225de885c6bbb8a
anno <anno@kali.kali.yerbas.org>
2023-11-14 01:44:17 -0800

    Next

--- php8v31.sh
+++ php8v31.sh
@@ -40,6 +40,12 @@ sed -i 's/extension=pdo_mysql/;extension=pdo_mysql/' /etc/php/8.1/fpm/php.ini
 sudo systemctl restart php8.1-fpm
 sudo apt update
 
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === 'e21205b207c3ff031906575712edab6f13eb0b361f2085f1f1237b7126d785e826a450292b6cfd1d64d92e6563bbde02') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php
+php -r "unlink('composer-setup.php');"
+mv composer.phar /usr/local/bin/composer
+
 sudo apt-get install openjdk-8-jdk -y
 echo "JAVA_HOME=$(which java)" | sudo tee -a /etc/environment
 source /etc/environment
