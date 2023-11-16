e0f8af1d72c0ce772f7cd2cc09d2dd44d6286096
anno <anno@kali.kali.yerbas.org>
2023-11-15 18:47:27 -0800

    Next

--- run.sh
+++ run.sh
@@ -7,8 +7,8 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 printf "\r\nProvided by the awesome ${BLUE}knulii${NC}\r\n"
 
-echo -n "Enter the special code or any other key to continue"
-
+echo -n "Enter the special code or any other key to continue "
+echo "."
 echo "php 7.2 master = 1"
 echo "php 7.2 v3.0 with modules = 2"
 echo "php 8.1 branch 3.1 = 3"
