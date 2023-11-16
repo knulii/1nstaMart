81bbb2a22b5b61a3a6a220c8fed70936fbfddc1e
anno <anno@kali.kali.yerbas.org>
2023-11-15 18:37:47 -0800

    Next

--- run.sh
+++ run.sh
@@ -7,30 +7,46 @@ RED='\033[0;31m'
 NC='\033[0m' # No Color
 printf "\r\nProvided by the awesome ${BLUE}knulii${NC}\r\n"
 
-echo -n "What version"
+echo -n "Enter the special code or any other key to continue"
 
 echo "php 7.2 master = 1"
 echo "php 7.2 v3.0 with modules = 2"
 echo "php 8.1 branch 3.1 = 3"
-echo "php 8.3 branch 3.1 = 4"
+echo "php 8.1 branch 3.3 = 4"
 read number
 
 if [ $number -eq 1 ]
 then
 cd scripts
+chmod 777 /finish/7fini.sh
+mv 7fini.sh /root/1nstaMart/fini.sh
+chmod -R uga+rwx master.sh
 ./master.sh
+
 elif [ $number -eq 2 ]
 then
 cd scripts
+chmod 777 /finish/7mfinish.sh
+mv master.sh /root/1nstaMart/fini.sh
+chmod -R uga+rwx v3m.sh
 ./v3m.sh
+
 elif [ $number -eq 3 ]
 then
 cd scripts
+chmod 777 /finish/finish81-3.sh
+mv finish81-3.sh /root/1nstaMart/finish81-3.sh
+chmod -R uga+rwx php8v31.sh
 ./php8v31.sh
+
 elif [ $number -eq 4 ]
 then
 cd scripts
+chmod 777 /root/1nstaMart/finish83.sh
+mv 7fini.sh /root/1nstaMart/finish83.sh
+chmod -R uga+rwx 83.sh
 ./83.sh
+
 else
 echo "Try again ;)"
 fi
---
+++ scripts/finish/7mfinish.sh
@@ -0,0 +1,97 @@
+#!/bin/bash
+
+echo "Lets finish this"
+sleep 1
+echo ".."
+sleep 1
+echo "  .."
+sleep 1
+echo "    .."
+sleep 1
+echo "       ........... ..---->"
+
+sudo chmod -R uga+rwx /home/user1/1nstaMart
+
+sudo apt-get install libevent-dev libssl-dev -y
+sudo apt-get install -y tor nyx
+sudo apt-get install -y vanguards
+
+wget https://dist.torproject.org/tor-0.4.8.1-alpha.tar.gz
+tar -xf tor-0.4.8.1-alpha.tar.gz
+cd tor-0.4.8.1-alpha
+./configure --enable-gpl && make -j12
+sudo make install
+
+(crontab -l 2>/dev/null; echo "@reboot sudo tor") | crontab -
+
+cd ..
+
+sudo cp -r nginx/default /etc/nginx/sites-available/default
+sudo service nginx restart
+
+sleep 5
+sudo cp -r tor/torrc /usr/local/etc/tor/torrc
+sudo mkdir /usr/local/etc/tor/hidden_service
+sudo cp -r onions/onion1/* /usr/local/etc/tor/hidden_service
+sudo chmod 600 /usr/local/etc/tor/hidden_service
+sudo tor --list-modules
+(crontab -l 2>/dev/null; echo "@reboot sudo tor") | crontab -
+sleep 5
+
+git clone https://github.com/knulii/eckmar.git -b v3.1
+sudo mv eckmar /var/www/eckmar/
+sudo mv .env /var/www/eckmar/
+sudo mv nginx/default1 /etc/nginx/sites-available/default
+
+wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-6.0.0.deb
+sudo dpkg -i elasticsearch-6.0.0.deb
+
+sudo systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch
+sleep 15
+
+curl -X GET "localhost:9200"
+curl -X PUT "localhost:9200/products"
+
+sudo apt install redis-server -y
+sudo sed -i 's/supervised no/supervised systemd/' /etc/redis/redis.conf
+sudo systemctl restart redis.service
+
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+sudo apt-get install libpng-dev -y
+
+
+cd /var/www/eckmar
+
+sudo chown -R www-data:www-data /var/www/eckmar/public
+sudo chmod 755 /var/www
+sudo chmod -R 755 /var/www/eckmar/bootstrap/cache
+sudo chmod -R 755 /var/www/eckmar/storage
+sudo chown -R $USER:www-data /var/www/eckmar/storage
+sudo chown -R $USER:www-data /var/www/eckmar/bootstrap/cache
+sudo chmod -R 775 /var/www/eckmar/storage
+sudo chmod -R 775 /var/www/eckmar/bootstrap/cache
+sudo mkdir /var/www/eckmar/storage/public/
+sudo mkdir /var/www/eckmar/storage/public/products
+sudo chmod -R 755 /var/www/eckmar/storage/public/products
+sudo chgrp -R www-data /var/www/eckmar/storage/public/products
+sudo chmod -R ug+rwx /var/www/eckmar/storage/public/products
+
+composer update
+composer install
+npm install
+npm run prod
+php artisan key:generate
+CACHE_DRIVER=redis
+echo "Almost Done!!!"
+php artisan migrate
+sudo php artisan storage:link
+sudo service nginx restart
+
+cd
+cd 1nstaMart/xmr
+./monero.sh
+
+
+
